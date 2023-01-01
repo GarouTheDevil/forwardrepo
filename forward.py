141eb60f67984df16439d16c3619cf93e628dccd
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-01-01 22:52:55 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,13 +1,13 @@
 
 from pyrogram import Client, filters
 
-bot = Client('Forward bot', api_id= "1543212", api_hash= "d47de4b25ddf79a08127b433de32dc84", bot_token= "1860781460:AAHOUPAWKur2BzMRfmLU1wIBedlm6GGGL8g")
+bot = Client('Forward bot', api_id= "1695333", api_hash= "acfb6f602239fc52a93d1847a3d850d1", bot_token= "5527448498:AAECjLx626Z5AaGOUZUIvIlq7rtWFQh6kDQ")
 
 @bot.on_message(filters.command('forward'))
 async def start(bot, message):
   try:
-    for i in range(4451, 5372):
-       await bot.copy_message(chat_id=-1001511163820, from_chat_id=-1001748041723,message_id=i)
+    for i in range(0, 5372):
+       await bot.copy_message(chat_id=-1001525314409, from_chat_id=-1001665685493,message_id=i)
   except:
        print("error")
 
