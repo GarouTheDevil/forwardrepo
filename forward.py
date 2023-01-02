93a16b32467574ca509754dc0328effcbbcad271
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-01-02 08:37:39 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -5,10 +5,16 @@ bot = Client('Forward bot', api_id= "1695333", api_hash= "acfb6f602239fc52a93d18
 
 @bot.on_message(filters.command('forward'))
 async def start(bot, message):
-  try:
-    for i in range(0, 5372):
-       await bot.copy_message(chat_id=-1001525314409, from_chat_id=-1001665685493,message_id=i)
-  except:
-       pass
+ # start_msg = await bot.ask(text = "Send The Start Message ID", chat_id = message.from_user.id, filters=filters.text, timeout=30)
+ # end_msg = await bot.ask(text = "Send The End Message ID", chat_id = message.from_user.id, filters=filters.text, timeout=30)
+ # bot.send_message(f'{start_msg} ,{end_msg}')
+  await bot.send_message(message.from_user.id,f"Forwading Started")
 
+  try:
+    for i in range(152, 12065):
+      await bot.copy_message(chat_id=-1001871401668, from_chat_id=-1001748041723,message_id=i)
+      time.sleep(5)
+  except Exception as e:
+       await bot.send_message(message.from_user.id,f"Forwading Stopped\n\nERROR : {e}")
+       
 bot.run()
