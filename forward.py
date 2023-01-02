206347f2d0cc00f7de0dc7861ea1bc8d8e4eda9a
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-01-02 09:04:49 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -12,7 +12,7 @@ async def start(bot, message):
 
   try:
     for i in range(0, 5372):
-      await bot.copy_message(chat_id=-1001871401668, from_chat_id=-1001748041723,message_id=i)
+      await bot.copy_message(chat_id=-1001525314409, from_chat_id=-1001665685493,message_id=i)
       time.sleep(5)
   except Exception as e:
        await bot.send_message(message.from_user.id,f"Forwading Stopped\n\nERROR : {e}")
