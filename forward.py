2f0348c66e6013e87cd16366becb8f95ab764ab9
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-06-28 21:42:42 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import pyromod.listen
-bot = Client('Forward bot', api_id= "1543212", api_hash= "d47de4b25ddf79a08127b433de32dc84", session_string= "BAAXjCwATVa3sVVvl3k-DW0pPjAyuTw8oH0hrHeu7ZA_x6yIhcynBUR87BHq3TQClack4i8dw4DuOkm2TDQFI5XaKgAd6kQ1_6ypNEdkm-3xEHMsotWEJhkUhzoja8V8990b7HMgucAj0H8nd689LT9lTrUvS7De5CCEy1Au3hDtiO-yUhrWjhid4SlGLVFTfy9PP_Wb7c8z5WMVbux2ep6VMxccvASXnFjJ2zTnuWY7Ma6URyzgKUkD43Aqy0d7KIpL_5kDGDTO2lUs_wRrrdKa8mj9y3-JttMY5-cc3DPbQF3OPHWIUJsgRFxamNan4fTPPg0kxr2aHnM8819HLbuqGlIxBQAAAABYHfJiAA")
+bot = Client('Forward bot', api_id= "1543212", api_hash= "d47de4b25ddf79a08127b433de32dc84", session_string= "BAAXjCwAb8M8WpFE8HpRFoCrU_fuMxxwJqAWggvr0oMIfbmAD30Wh2H91lbjz_I7eU0ULltGD2X4X0nrP2qCfQINuoTT6vOZX3aYAA6cZuKAwyN9dfBcjw8rMl8Nukn3dgbO97AG7bB2vQs17Xv5bVChyHOP9BsiyVHZd3wIIUcVwkrs3byXKAQNsHQeYgDNBiNWrgVEcYqICymfxRlExsyf6dqFmRWHDF-yJ-4lNn28JziGDm3mbyP3uwTKlBPGI0ME3I_tx94Pd5iejsCMFdd5q_1TLUG9y8jsMvbBSK3Orm0VWgAyM4gRXEwt6a8J0i3a_JI1J1OxykLlvRQlx3rvWCCsjQAAAABYHfJiAA")
 import time
 
 @bot.on_message(filters.command('forward'))
@@ -11,7 +11,7 @@ async def start(bot, message):
   await bot.send_message(message.from_user.id,f"Forwading Started")
 
   try:
-    for i in range(1783, 4166):
+    for i in range(2052, 4166):
       await bot.copy_message(chat_id=-1001525314409, from_chat_id= -1001260881403,message_id=i)
       time.sleep(5)
   except:
