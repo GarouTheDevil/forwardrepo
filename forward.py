49650edb94f889a16c8d02348606f25347ab7aeb
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-06-28 13:20:31 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import pyromod.listen
-bot = Client('Forward bot', api_id= "1695333", api_hash= "acfb6f602239fc52a93d1847a3d850d1", bot_token= "5527448498:AAGXvarac-1CXLl-7b1EtjvKDyvsdD0RSYA")
+bot = Client('Forward bot', api_id= "1543212", api_hash= "d47de4b25ddf79a08127b433de32dc84", session_string= "BAAXjCwAEXawe2BfRCJvUc5m1oUz3p-yUzdxpBZtVU_C2OurynoMQwzgjSJZNv8WXoUJg-fpkupC27LV0RTKELZr5talUKO3W8hm_1eqQcSCd_c_ZHwCxl3gVVzTCeiufJzcnHxtsXFwNdFxovANu6jOZrJdmLBElZWdHDcCdlx43iEFCmC51d1wKNZ0_K_uagRHQPyBkf68xZuo2olvpc2OxA41WwHgXv0Se6tU6VQPQIsUkWnsNl9v_wdMaTMusfy_OqO5kxx_ocWHKpD-46_of5Q6XkIbyn9iOvlkSZmdIWlIt1GB0JMo8tA9fVsLUZaZmPGWXUhs_-aw5jpKnjramYuRkQAAAABYHfJiAA")
 import time
 
 @bot.on_message(filters.command('forward'))
@@ -11,8 +11,8 @@ async def start(bot, message):
   await bot.send_message(message.from_user.id,f"Forwading Started")
 
   try:
-    for i in range(330, 5372):
-      await bot.copy_message(chat_id=-1001525314409, from_chat_id=-1001665685493,message_id=i)
+    for i in range(0, 4166):
+      await bot.copy_message(chat_id=-1001525314409, from_chat_id= -1001260881403,message_id=i)
       time.sleep(5)
   except:
        pass
