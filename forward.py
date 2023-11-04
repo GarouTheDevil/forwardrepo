e172684c763736628576aa1f62f242156c06f3ae
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2023-11-04 07:42:45 +0530

    Update forward.py

--- forward.py
+++ forward.py
@@ -12,7 +12,7 @@ async def start(bot, message):
 
   try:
     for i in range(5, 5688):
-      await bot.copy_message(chat_id=-1001959628938, from_chat_id= -1001260881403,message_id=i)
+      await bot.copy_message(chat_id=-1001525314409, from_chat_id=-1001959628938,message_id=i)
       time.sleep(5)
   except:
        pass
